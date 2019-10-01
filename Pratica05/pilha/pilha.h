ad0e7e1ccd6f33cb427cdaabb125a4046aed3d66
marcos <leaomaritnshenrique@gmail.com>
2019-10-01 20:42:13 -0300

    preparando arquivos para pratica05

---
+++ Pratica05/pilha/pilha.h
@@ -0,0 +1,48 @@
+#ifndef PILHA_H_
+#define PILHA_H_
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Pilha{
+private:
+    T * itens = NULL;
+    int capacidade;
+    int topo;
+public:
+    Pilha(int cap) : capacidade(cap),  topo(0) {
+        itens = new T[cap]();
+    }
+
+    ~Pilha() {
+        delete []itens;
+    }
+
+    void empilha(T item) {
+        if(topo < capacidade) {
+            itens[topo] = item;
+            // cout<<"empilhando: "<<itens[topo]<<endl;
+            topo++;
+        } else {
+            cout<<"Estouro de pilha!";
+        }
+    }
+
+    T desempilha() {
+        T dado = 0;
+        if (topo > 0)
+        {
+            topo--;
+            dado = itens[topo];
+            // cout<<"desempilhando: "<<itens[topo]<<endl;
+        } else
+        {
+            cout<<"Pilha vazia!";
+        }
+        return dado;
+    }
+};
+
+#endif
